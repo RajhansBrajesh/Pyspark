2bda4b8f9e6766e1212487557283385af3d5bf5a
RajhansBrajesh <72440903+RajhansBrajesh@users.noreply.github.com>
2025-06-24 21:40:48 +0530

    Create databricks_bronze_silver.py

---
+++ databricks_bronze_silver.py
@@ -0,0 +1,22 @@
+df_raw = spark.read.option("header", "true").csv(file_path)
+df = df_raw.withColumn("ADLS_LOADED_DATE", F.lit(triggerdate))
+
+#To replace special chartecter in column names
+df = df.select([F.col(col).alias(col.replace('-','')) for col in df.columns])
+
+# generally we use df_raw to write in Bronze and Silver both as written below
+df.write.option('mergeSchema', 'true').saveAsTable(BronzeTableName, mode = 'append')
+stagingTable = "stg_" +objectName
+df.createOrReplaceTempView(stagingTable)
+Source = stagingTable
+Target = prod_l1.wpc.tableName
+if extratiobType.lower() == 'full':
+  df.write.option('mergeSchema', 'true').saveAsTable(SilverTableName, mode = 'overwrite')
+else:
+  spark.sql("""
+            MERGE INTO {Target} AS target
+            USING {Source} AS source
+            ON target.id = source.id
+            WHEN MATCHED THEN UPDATE SET *
+            WHEN NOT MATCHED THEN INSERT *
+            """)
