06981efceb66fd1e665c6d500901c637051481c0
RajhansBrajesh <72440903+RajhansBrajesh@users.noreply.github.com>
2025-06-24 22:55:02 +0530

    Update databricks_bronze_silver.py

--- databricks_bronze_silver.py
+++ databricks_bronze_silver.py
@@ -13,6 +13,8 @@ Target = prod_l1.wpc.tableName
 if extratiobType.lower() == 'full':
   df.write.option('mergeSchema', 'true').saveAsTable(SilverTableName, mode = 'overwrite')
 else:
+   # Below code will automatically merge the schema as spark config is True
+  spark.conf.set("spark.databricks.delta.schema.autoMerge.enabled", "true")
   spark.sql("""
             MERGE INTO {Target} AS target
             USING {Source} AS source
@@ -20,3 +22,21 @@ else:
             WHEN MATCHED THEN UPDATE SET *
             WHEN NOT MATCHED THEN INSERT *
             """)
+
+  # There is another way of doing the same:
+    spark.sql("""
+            MERGE INTO {Target} AS target
+            USING {Source} AS source
+            ON target.id = source.id
+            WHEN MATCHED THEN DELETE
+            """)
+   df.write.option('mergeSchema', 'true').saveAsTable(SilverTableName, mode = 'append')
+
+
+
+
+
+
+
+
+
