0d032ceb17b9ee6ab07814703a9deb04d69e2716
RajhansBrajesh <72440903+RajhansBrajesh@users.noreply.github.com>
2025-07-02 19:53:24 +0530

    Update databricks_bronze_silver.py

--- databricks_bronze_silver.py
+++ databricks_bronze_silver.py
@@ -86,6 +86,110 @@ WHEN MATCHED AND source._change_type = 'delete' THEN
 WHEN NOT MATCHED AND source._change_type = 'insert' THEN
   INSERT *;
 
+############################ SAP CDC implementation in use ########################
+
+def replace_none_from_primarykey_col(df, col_list =[]):
+  """ Remove null with NA values in pk col"""
+  df = df.na.fill('NA', col_list)
+  return df
+
+def is_delta_table_available(target_delta_table) -> bool:
+  return len(target_delta_table.toDF().schema.fields)>0
+
+def get_merge_condition(pk_col_list):
+  merge_condition = ''
+  if len(pk_col_list)>0:
+    left_col = []
+    right_col = []
+    for pk in pk_col_list:
+      left_col.append(f"s1.{pk}")
+      right_col.append(f"t1.{pk}")
+
+merge_condition = [i+ ' = ' + j for i,j in zip(left_col,right_col)]
+join_columns = ''
+for cols in merge_condition:
+  join_columns = join_columns _cols + ' And '
+
+merge_condition = join_columns[:-4]
+return merge_condition
+
+# Function to load from bronze to silver
+def updert_to_delta(batch_df, batch_id):
+  ''' Here try and except with raise Exception and terminates the notebook'''
+  
+  try:
+    tgt_delta_table = DeltaTable.forName(spark, silver_table_name)
+    window_spec = Window.partitionNy(primary_key_col_list).orderBy(col("_processed_time").desc())
+    batch_df = batch_df.withColumn("latest_record", row_number().over(window_spec)\
+                                   .filter("latest_record ==1")\
+                                   .drop("latest_record")
+
+   if extraction_type == 'full':
+      batch_df.write.option('mergeSchema', 'true').saveAsTable(SilverTableName, mode = 'overwrite')
+  else:
+    if is_delta_table_available(tgt_delta_table):
+      merge_condition = get_merge_condition()
+      tgt_deltatable.alias("t1").merge(batch_df.alias("s1"), merge_condition) \
+                                .whenMatchedDelete("s1._operation_type = 'D'") \
+                                .whenMatchUpdateAll("s1._operation_type != 'D'") \
+                                .whenNotMatchInsertAll("s1._operation_type != 'D'") 
+                                .execute()
+  else:
+   batch_df.write.option('mergeSchema', 'true').saveAsTable(SilverTableName, mode = 'overwrite')
+
+  except Exception as error:
+    raise Exception(f'Error occurred while merging, error msg: {error}')
+
+
+## Read bronze Table
+read_bronze_df = spark.readstream.option("ignoreDeletes", "true").table(bronze_table_name)
+
+if len(primary_key_col_list)>0:
+  read_bronze_df = replace_none_from_primarykey_col(read_bronze_df, priamary_key_col_list)
+
+##Write to silver table
+
+read_bronze_df.writeStream\
+              .format("delta")\
+              .outputMode("update")\
+              .option("mergeSchema", "true")\
+               .option("chekpointlocation", checkpoint_location)\
+               .option("failOnDataLoss" False)\
+               .trigger(availableNow = true)
+               .foreachBatch(upsert_to_delta)\
+               .start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
